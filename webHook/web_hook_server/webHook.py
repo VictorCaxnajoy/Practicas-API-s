4268b25604bd771da97e925022597351879f7c75
Victor Caxnajoy <victoralva037@gmail.com>
2025-07-15 16:36:00 -0600

    version 2

--- webHook/web_hook_server/webHook.py
+++ webHook/web_hook_server/webHook.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from schemas.user_schema import user_schema
 import requests
-import json
+import json 
 
 
 app = FastAPI()
 
 @app.post("/webhook")
 def newUser(user: user_schema):
-    response = requests.post(user.url_user, data=json.dumps(user), headers={'Content-Type' : 'application/json'})
+    response = requests.post(user.url_user, data=json.dumps("usuario Creado"), headers={'Content-Type' : 'application/json'})
